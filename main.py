a7b2c68650cfa82046a55d78a477381f1bd003b3
MortInsane <mort.insane@gmail.com>
2021-09-13 07:24:14 +0300

    changed main

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from vk import VK
 from ya_disk import YandexDisk
 import os
 
-TOKEN = ...
+VK_TOKEN = ...
 Yandex_TOKEN = ...
 username = ...
-
+version = "5.131"
 files_path = os.path.join(os.getcwd(), 'files')
 
-v = VK(token=TOKEN, user=username, version="5.131")
+
+v = VK(token=VK_TOKEN, user=username, version=version, count_to_upload=3)
 v.collection_data()
 
 ya = YandexDisk(token=Yandex_TOKEN, json_path=files_path, user_folder=username)
